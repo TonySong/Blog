655edf1061f7f33588d2958da5450bb464b985a3
Tonysong <performerosng@gmail.com>
2011-12-12 16:52:59 -0600

    inital commit

---
+++ Blog/app/helpers/posts_helper.rb
@@ -0,0 +1,7 @@
+module PostsHelper
+  
+  def join_tags(post)     
+    post.tags.map { |t| t.name }.join(", ")   
+  end 
+    
+end
---
+++ Blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c39e890b8a0c309b0191388fe87367bfdaab57aebf6b99684fdb0935409b06edb40991cbfcb7b758153ff1606e293cbffbdc19481bd8c7da2b1b9b45931365da'
