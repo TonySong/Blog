e4bff8bf6d16e883633981b6a16bab0e3c06659e
Tonysong <performerosng@gmail.com>
2011-12-15 10:23:16 -0600

    add hello prj

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'be3981f20ea6b4766eb6daacfad7bb6f7c633f8d0e726c3a9b9a30ce07b833e433f5928054658384e7eb997a830c6610254e6bec885e8aea5977b2060fd8607d'
